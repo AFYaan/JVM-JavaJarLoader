00964c2c16039173f01bfecea936437ccaefc3df
Adrian Karski <afyaan321@gmail.com>
2021-03-10 01:51:56 +0100

    Added license

--- Resources.h
+++ Resources.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 AFYaan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License atv
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const unsigned char rawData[2416] = {
     0xA5, 0xA0, 0xD6, 0x0F, 0xD0, 0xD4, 0xE4, 0x07, 0x45, 0xFB, 0xB8, 0x35, 0x4E, 0x3B, 0x0C, 0xD0, 0xE0, 0x29,
     0x0F, 0xDB, 0x56, 0xA0, 0x1E, 0x6F, 0x4E, 0x04, 0x7E, 0xD6, 0x47, 0xE1, 0xD8, 0x59, 0xF3, 0x9A, 0x7E, 0x8D,
